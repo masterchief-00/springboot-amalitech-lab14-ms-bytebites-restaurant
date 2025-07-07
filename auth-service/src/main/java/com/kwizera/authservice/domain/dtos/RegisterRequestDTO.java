1d7c04e8e58a5c41b257af0667467304580798f2
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-07-07 12:51:24 +0200

    fix: added eureka client dependency to api gateway service to solve service discovery issues

--- auth-service/src/main/java/com/kwizera/authservice/domain/dtos/RegisterRequestDTO.java
+++ auth-service/src/main/java/com/kwizera/authservice/domain/dtos/RegisterRequestDTO.java
@@ -1,6 +1,7 @@
 package com.kwizera.authservice.domain.dtos;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -11,6 +12,7 @@ public record RegisterRequestDTO(
         String firstName,
         @Pattern(regexp = "^[A-Za-z0-9 ]{2,50}$", message = "Name must contain only alphanumeric characters")
         String lastName,
+        @NotNull(message = "Invalid email")
         @Email(message = "Invalid email")
         String email,
         @Size(min = 8, message = "The needs to be at least 8 characters")
