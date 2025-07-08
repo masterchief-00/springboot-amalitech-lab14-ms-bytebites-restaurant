9135f63ea82e6653030523c37331aa253d93c8f0
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-07-08 16:17:21 +0200

    chore: added the entities and repositories in the order service

---
+++ order-service/src/main/java/com/kwizera/orderservice/domain/entities/OrderItem.java
@@ -0,0 +1,28 @@
+package com.kwizera.orderservice.domain.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@Builder
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "food_id", nullable = false)
+    private Long foodId;
+    @Column(name = "food_name", nullable = false)
+    private String foodName;
+    @Column(nullable = false)
+    private int quantity;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    @JsonIgnore
+    private Order order;
+}
